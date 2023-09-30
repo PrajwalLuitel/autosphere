acedc6a6ae4b5888dc7ed22478094fd4c657a461
PrajwalLuitel <prajwalluitel@gmai.com>
2023-09-30 11:13:14 +0545

    CarCards and CardDetails added

--- utils/index.ts
+++ utils/index.ts
@@ -1,28 +1,45 @@
+import { CarProps } from "@/types";
+
 export async function fetchCars() {
-    const headers = {
-            'X-RapidAPI-Key': '5ef2b39154msh39d7b565703b601p10528ajsn60d0b31d9cb3',
-            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+  const headers = {
+    "X-RapidAPI-Key": "5ef2b39154msh39d7b565703b601p10528ajsn60d0b31d9cb3",
+    "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+  };
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=huracan",
+    {
+      headers: headers,
     }
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla',
-        {
-            headers: headers,
-        });
-    const result = await response.json();
-    return result;
+  );
+  const result = await response.json();
+  return result;
 }
 
-
 export const calculateCarRent = (city_mpg: number, year: number) => {
-    const basePricePerDay = 50; // Base rental price per day in dollars
-    const mileageFactor = 0.1; // Additional rate per mile driven
-    const ageFactor = 0.05; // Additional rate per year of vehicle age
-  
-    // Calculate additional rate based on mileage and age
-    const mileageRate = city_mpg * mileageFactor;
-    const ageRate = (new Date().getFullYear() - year) * ageFactor;
-  
-    // Calculate total rental rate per day
-    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
-  
-    return rentalRatePerDay.toFixed(0);
-  };
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const generateCarImageUrl = (car: CarProps, angle?: string) => {
+  const url = new URL("https://cdn.imagin.studio/getimage");
+  const { make, year, model } = car;
+
+  url.searchParams.append("customer", "hrjavascript-mastery");
+  url.searchParams.append("make", make);
+  url.searchParams.append("modelFamily", model.split(" ")[0]);
+  url.searchParams.append("zoomType", "fullscreen");
+  url.searchParams.append("modelYear", `${year}`);
+  url.searchParams.append("angle", `${angle}`);
+
+  return `${url}`;
+};
